c6b6379c598a85f96afac1df31d041c32ccae6ab
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-02-12 21:53:15 +0300

    homework 2

---
+++ Homework_2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задать массив, заполнить случайными положительными трёхзначными числами.
+// Показать количество нечетных\четных чисел
+int[] a = new int[100];
+ 
+Random random = new Random();
+for (int i = 0; i < a.Length; i++)
+a[i] = random.Next(100, 1000);
+ 
+int c = a.Count(x => x % 2 == 0);
+int n = a.Count(x => x % 2 != 0);
+            
+Console.WriteLine($"Сам массив {string.Join(", ", a)}\nЧетные {c}\nНечетные {n}");
+
