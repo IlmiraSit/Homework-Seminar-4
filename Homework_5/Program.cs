02e561ac2e33bf2edd2b60382c4018915ebdd19b
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-02-12 22:02:14 +0300

    homework 5

---
+++ Homework_5/Program.cs
@@ -0,0 +1,17 @@
+﻿//Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+ Console.WriteLine("Array Size: "); 
+int n; 
+n = Convert.ToInt32(Console.ReadLine()); 
+int[] ar = new int[n]; 
+var rand = new Random(); 
+       
+for(int i = 0; i < ar.Length; i++) 
+ar[i] = rand.Next(-100, 100); 
+Console.Write("Values of array elements: "); 
+foreach(var item in ar) 
+Console.Write("{0} ", item); 
+Console.WriteLine(); 
+for(int i = 0; i < ar.Length / 2; i++) 
+Console.WriteLine("element {0} * element {1}, {2} * {3}  = {4}", i, ar.Length - i - 1, ar[i], ar[ar.Length - i - 1], ar[i] * ar[ar.Length - i - 1]); 
+return; 
+
