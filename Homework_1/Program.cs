a214197f8a1cd547c6dd87687ced418752923c3d
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-02-12 21:48:04 +0300

    homework 1

---
+++ Homework_1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int i;
+int[] a = new int[8];
+Console.WriteLine("Введите массив чисел: ");
+for (i = 0; i < 8; i++)
+{
+a[i] = int.Parse(Console.ReadLine());
+}
+ 
+for (i = 0; i < 8; i++)
+{
+Console.WriteLine(a[i]);
+}
+
