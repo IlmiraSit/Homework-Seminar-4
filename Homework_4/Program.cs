bfcd9c8fdec90d16b71a04bdfaf3b61aac31fab4
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-02-12 21:58:51 +0300

    homework 4

---
+++ Homework_4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int[] a = new int[10];
+for (int i = 0; i < a.Length; i++)
+{
+    Console.Write($"Введите a[{i}] = ");
+    a[i] = int.Parse(Console.ReadLine());
+}
+int sum = 0;
+for (int i = 0; i < a.Length; i++)
+{
+    if (a[i] % 2 == 1)
+    sum += a[i];
+}
+Console.WriteLine($"Сумма нечетных чисел = {sum}");
+
